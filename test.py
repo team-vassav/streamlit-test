8c07e990907c28b638d5234c2e8f8a440e609ca7
VASSAV <teamvassav@gmail.com>
2022-04-16 20:06:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,5 @@ from PIL import Image
 
 st.write(""" # Welcome to VASSAV Test App ! # """)
 
-cv2_image = cv2.imread("Apple.jpg")
-cv2_convert = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("Gray_Apple.jpg",cv2_convert)
 
-with open("Gray_Apple.jpg", "rb") as image:
-    st.download_button(label="OUTPUT", data=image, file_name="output.png", mime="image/png")
     
