f3756edd3ea0c27064a05ba7c91162614a613d79
VASSAV <teamvassav@gmail.com>
2022-04-16 19:43:25 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+
+cv2_image = cv2.imread("Apple.jpg")
+cv2_convert = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("Gray_Apple.jpg",cv2_convert)
+
+with open("Gray_Apple.jpg", "rb") as image:
+    st.download_button(label="OUTPUT", data=image, file_name="output.png", mime="image/png")
+    
